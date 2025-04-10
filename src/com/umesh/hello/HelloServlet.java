08bfa4fd64027a2da1b487178ac547848ef8a807
umesh raul <umeshraul@umeshs-MacBook-Air.local>
2025-04-10 23:51:41 +0530

    Second Deployment GoCD

--- src/com/umesh/hello/HelloServlet.java
+++ src/com/umesh/hello/HelloServlet.java
@@ -17,6 +17,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<p>Ready for the Change!</p>");
         out.println("<p>Now tracked via Git and pushed to GitHub!</p>");
         out.println("<p>Deployed via GoCD at 10:25:00 PM!</p>");
+        out.println("<p>Second Deployment via GoCD at 11:49:00 PM!</p>");
     }
 }
 
