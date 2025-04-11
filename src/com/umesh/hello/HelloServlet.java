72855a3ce159e6ddf8fcab50057d44a64a791215
umesh raul <umeshraul@umeshs-MacBook-Air.local>
2025-04-11 06:41:54 +0530

    CICD Deployment

--- src/com/umesh/hello/HelloServlet.java
+++ src/com/umesh/hello/HelloServlet.java
@@ -19,6 +19,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<p>Deployed via GoCD at 10:25:00 PM!</p>");
         out.println("<p>Second Deployment via GoCD at 11:49:00 PM!</p>");
         out.println("<p>Third Automated Deployment via GoCD at 00:30:00 AM by AUR!</p>");
+        out.println("<p>Fourth CD/CD Automated Deployment via GoCD at 06:40:00 AM!</p>");
     }
 }
 
